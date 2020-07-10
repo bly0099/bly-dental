a5438ccef971e6d212bac1137ae7cf9964ac7eec
haru <haaaru5500@gmail.com>
2020-07-10 20:34:51 +0900

    haaaru変更しました

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,7 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
 // import Img from "gatsby-image"
-
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import {
   faTwitter,
