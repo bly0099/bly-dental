437b60cd92e645bb1f112ba7458a030b81014239
haaaru44 <haaaru4400@gmail.com>
2020-08-18 16:30:26 +0900

    meta

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,7 @@
 module.exports = {
   /* Your site config here */
   siteMetadata: {
-    title: `Bly`,
+    title: `Bly'`,
     description: `ブリー`,
     lang: `ja`,
     siteUrl: `https://bly-dental-japan.com`,
